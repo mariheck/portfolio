569aa56276438567ca257d55a92f21627a77849a
Marine Heckler <marine.heckler@gmail.com>
2020-06-24 12:29:00 +0200

    Add Card component.

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './card.styles.scss';
+
+const Card = ({
+    id,
+    projectsNumber,
+    title,
+    imageUrl,
+    description,
+    websiteUrl,
+    githubLink
+}) => (
+    <div className="card">
+        <div
+            className="image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+        />
+        <div className="content">
+            <span>
+                {id}/{projectsNumber}
+            </span>
+            <h4>{title}</h4>
+            <p>{description}</p>
+            <p className="links">
+                <a
+                    href={websiteUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Voir le site"
+                >
+                    <i className="external alternate icon"></i>
+                </a>
+                <a
+                    href={githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Code source"
+                >
+                    <i className="github icon"></i>
+                </a>
+            </p>
+        </div>
+    </div>
+);
+
+export default Card;
--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,82 +1,70 @@
 import React from 'react';
+import Card from '../../components/card/card.component';
 import './projects.styles.scss';
 
-const ProjectsPage = () => {
-    const imageUrl =
-        'https://images.unsplash.com/photo-1592917907394-e016cbbb6c5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80';
-    return (
-        <main>
-            <h3>Projets</h3>
-            <p className="intro">
-                Vous trouverez ici certains de mes travaux passés, parmis ceux
-                qui m'ont le plus apporté, ou tout simplement parmis mes
-                favoris. N'hésitez pas à consulter mon profil GitHub pour voir
-                plus de projets.
-            </p>
-            <div className="grid">
-                <div className="card">
-                    <div
-                        className="image"
-                        style={{ backgroundImage: `url(${imageUrl})` }}
-                    />
-                    <div className="content">
-                        <span>0001/003</span>
-                        <h4>Projet 1</h4>
-                        <p>
-                            texte texte texte texte texte texte texte texte
-                            texte texte texte texte texte texte texte texte
-                        </p>
-                        <button>
-                            <i className="large github icon"></i>
-                        </button>
-                        <button>
-                            <i className="large external alternate icon"></i>
-                        </button>
-                    </div>
+class ProjectsPage extends React.Component {
+    state = {
+        projects: [
+            {
+                id: 1,
+                title: 'Saving',
+                imageUrl: './images/saving.jpg',
+                description:
+                    'texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte',
+                websiteUrl: 'https://github.com',
+                githubLink: 'https://github.com/mariheck'
+            },
+            {
+                id: 2,
+                title: 'Saving',
+                imageUrl: './images/saving.jpg',
+                description:
+                    'texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte',
+                websiteUrl: 'https://github.com',
+                githubLink: 'https://github.com/mariheck'
+            },
+            {
+                id: 3,
+                title: 'Saving',
+                imageUrl: './images/saving.jpg',
+                description:
+                    'texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte texte',
+                websiteUrl: 'https://github.com',
+                githubLink: 'https://github.com/mariheck'
+            }
+        ]
+    };
+
+    render() {
+        const { projects } = this.state;
+        const projectsNumber = projects.length;
+
+        return (
+            <main>
+                <h3>Projets</h3>
+                <p className="intro">
+                    Vous trouverez ici certains de mes travaux passés, parmis
+                    ceux qui m'ont le plus apporté, ou tout simplement parmis
+                    mes favoris. N'hésitez pas à consulter mon profil GitHub
+                    pour voir plus de projets.
+                </p>
+                <div className="grid">
+                    {projects.map(project => (
+                        <Card
+                            key={project.id}
+                            id={`00${project.id}`}
+                            projectsNumber={`00${projectsNumber}`}
+                            title={project.title}
+                            imageUrl={project.imageUrl}
+                            description={project.description}
+                            websiteUrl={project.websiteUrl}
+                            githubLink={project.githubLink}
+                        />
+                    ))}
                 </div>
-                <div className="card">
-                    <div
-                        className="image"
-                        style={{ backgroundImage: `url(${imageUrl})` }}
-                    />
-                    <div className="content">
-                        <span>0002/003</span>
-                        <h4>Projet 2</h4>
-                        <p>
-                            texte texte texte texte texte texte texte texte
-                            texte texte texte texte texte texte texte texte
-                        </p>
-                        <button>
-                            <i className="large github icon"></i>
-                        </button>
-                        <button>
-                            <i className="large external alternate icon"></i>
-                        </button>
-                    </div>
-                </div>
-                <div className="card">
-                    <div
-                        className="image"
-                        style={{ backgroundImage: `url(${imageUrl})` }}
-                    />
-                    <div className="content">
-                        <span>0003/003</span>
-                        <h4>Projet 3</h4>
-                        <p>
-                            texte texte texte texte texte texte texte texte
-                            texte texte texte texte texte texte texte texte
-                        </p>
-                        <button>
-                            <i className="large github icon"></i>
-                        </button>
-                        <button>
-                            <i className="large external alternate icon"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </main>
-    );
-};
+            </main>
+        );
+    }
+}
 
 export default ProjectsPage;
