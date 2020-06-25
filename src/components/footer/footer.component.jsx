229ea2c5d7d1beb75eca01f0fe45aad4bc69e744
Marine Heckler <marine.heckler@gmail.com>
2020-06-25 10:48:46 +0200

    Minor stylistic changes.

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
     return (
         <footer>
             <p>
-                {year} &copy; Marine Heckler | Siret : 880 456 595 00011 |{' '}
-                <Link to="/mentions">Mentions Légales</Link>
+                {year} <span>&copy;</span> Marine Heckler | Siret : 880 456 595
+                00011 | <Link to="/mentions">Mentions Légales</Link>
             </p>
         </footer>
     );
