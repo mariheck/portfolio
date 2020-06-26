49e2b05a81b5b522b0368d891aedfb2652d0811a
Marine Heckler <marine.heckler@gmail.com>
2020-06-26 12:31:44 +0200

    Implement home page.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './homepage.styles.scss';
 
-const HomePage = () => <div>Home Page</div>;
+const HomePage = () => (
+    <React.Fragment>
+        <div
+            className="home-page-background"
+            style={{
+                backgroundImage:
+                    'url(https://images.unsplash.com/photo-1590157678696-a5151f512cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80)'
+            }}
+        ></div>
+        <div className="home-page-text">
+            <h5>Marine Heckler</h5>
+            <h3>Développement de Sites Web</h3>
+            <div className="links-wrapper">
+                <Link to="/services">En savoir plus</Link>
+                <a href="mailto:marine.heckler@icloud.com">Me contacter</a>
+            </div>
+        </div>
+    </React.Fragment>
+);
 
 export default HomePage;
--- src/pages/mentions/mentions.component.jsx
+++ src/pages/mentions/mentions.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const MentionsPage = () => (
-    <div>
-        Mentions Page <br /> https://unsplash.com/@lephotographe_
-    </div>
+    <main>
+        <h3>Mentions Légales</h3>
+        <div className="container">https://unsplash.com/@lephotographe_</div>
+    </main>
 );
 
 export default MentionsPage;
