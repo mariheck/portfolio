18baa130e76139b8eb4c6497139b275df37dd1b9
Marine Heckler <marine.heckler@gmail.com>
2020-07-21 15:29:40 +0200

    Updates

---
+++ precache-manifest.80d57c6aeed13fdb713e78df43f2d785.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baccbe1e7c87edc4437cb795838b3da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d99f166a5bf81bfb55ed",
+    "url": "/portfolio/static/css/2.5aa7db39.chunk.css"
+  },
+  {
+    "revision": "1f6983c78a387837021f",
+    "url": "/portfolio/static/css/main.5b0e38d9.chunk.css"
+  },
+  {
+    "revision": "d99f166a5bf81bfb55ed",
+    "url": "/portfolio/static/js/2.a347af24.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/portfolio/static/js/2.a347af24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6983c78a387837021f",
+    "url": "/portfolio/static/js/main.6dc457dc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c53425dfe84a897732e5a07baa15083b",
+    "url": "/portfolio/static/media/back.c53425df.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "5615b5ee1c74f0782e8bc5ac0da8e919",
+    "url": "/portfolio/static/media/front.5615b5ee.jpg"
+  },
+  {
+    "revision": "d4ae68e6d2fb18132d55b05be76e4b31",
+    "url": "/portfolio/static/media/homepage.d4ae68e6.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5e56ddee8de3b4c7771cbf6d9e1939e7",
+    "url": "/portfolio/static/media/mobile.5e56ddee.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b1466a7358e149701c22ce78f329887d",
+    "url": "/portfolio/static/media/portrait.b1466a73.jpg"
+  },
+  {
+    "revision": "3fdb5c97b6f4fb3fefed370a6d6d1e4b",
+    "url": "/portfolio/static/media/saving.3fdb5c97.jpg"
+  },
+  {
+    "revision": "0d858c4440a16af2e1ae369e192cefc1",
+    "url": "/portfolio/static/media/smartbrain.0d858c44.jpg"
+  },
+  {
+    "revision": "b2a5030bc5c4cc6b3e040a8090bc0e8f",
+    "url": "/portfolio/static/media/zentogo.b2a5030b.jpg"
+  }
+]);
