1f2c95c170a99eec74ef3989c2ae56303075afbd
Marine Heckler <marine.heckler@gmail.com>
2020-07-21 17:02:15 +0200

    Compress images + Add React.lazy and spinner for pages upload.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import MainPage from './pages/main-page/main-page.component';
-import MentionsPage from './pages/mentions/mentions.component';
 import Footer from './sections/footer/footer.component';
+import Spinner from './components/spinner/spinner.component';
+
+const MainPage = lazy(() => import('./pages/main-page/main-page.component'));
+const MentionsPage = lazy(() => import('./pages/mentions/mentions.component'));
 
 function App() {
     return (
         <div>
             <Switch>
-                <Route exact path="/portfolio" component={MainPage} />
-                <Route
-                    exact
-                    path="/portfolio/mentions"
-                    component={MentionsPage}
-                />
+                <Suspense fallback={<Spinner />}>
+                    <Route exact path="/portfolio" component={MainPage} />
+                    <Route
+                        exact
+                        path="/portfolio/mentions"
+                        component={MentionsPage}
+                    />
+                </Suspense>
             </Switch>
             <Footer />
         </div>
